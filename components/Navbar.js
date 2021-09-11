a9d8078aec50f0f0bfc5b946b85e626a59819280
farazzhh <havaee.faraz@gmail.com>
2021-09-12 01:38:08 +0430

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,29 @@
+import Link from "next/link";
 const Navbar = () => {
   return (
-    <nav className="navbar h-24 w-screen border shadow-sm">
+    <nav className="navbar h-24 w-screen border shadow-sm px-8">
       <div className="container mx-auto flex justify-between items-center h-full">
         <h1 className="font-bold ">
-          <a href="#" className="text-shadow">
-            NextApp
-          </a>
+          <Link href="/">
+            <a className="text-shadow">NextApp</a>
+          </Link>
         </h1>
         <div className="nav-menu flex gap-4 text-gray-600 font-light">
-          <a
-            href="#"
-            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
-          >
-            Home
-          </a>
-          <a
-            href="#"
-            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
-          >
-            About
-          </a>
-          <a
-            href="#"
-            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
-          >
-            myNextApp
-          </a>
+          <Link href="/">
+            <a className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1">
+              Home
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1">
+              About
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1">
+              myNextApp
+            </a>
+          </Link>
         </div>
       </div>
     </nav>
