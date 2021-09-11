4ad8c2161d889b6228c0059f176e8f51f2ca1fe8
farazzhh <havaee.faraz@gmail.com>
2021-09-12 01:43:18 +0430

    copyright

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,16 @@ const Footer = () => {
       <footer className="navbar h-24 w-screen border shadow-sm px-8">
         <div className="container mx-auto flex justify-between items-center h-full">
           <h1 className="">
-            <a href="#" className="text-shadow">
+            <a href="#" className="text-shadow ">
               NextApp
             </a>
           </h1>
 
-          <p className="text-shadow">Copyright 2021 Farazzhh</p>
-          <a href="" className="text-shadow">
-            Github
+          <p className="text-shadow text-sm font-light">
+            Copyright 2021 Farazzhh
+          </p>
+          <a href="https://github.com/farazzhh" target="blank"  className="text-shadow">
+            myGithub
           </a>
         </div>
       </footer>
