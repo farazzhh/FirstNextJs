3b9ef2b953d41f63915cd0ce1cd42039da1757ae
farazzhh <havaee.faraz@gmail.com>
2021-09-12 01:17:13 +0430

    nav shadow

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,35 @@
 const Navbar = () => {
-    return (
-      <nav className="navbar h-24 w-screen border">
-        <div className="container mx-auto flex justify-between items-center h-full">
-          <h1 className="font-black ">
-            <a href="#" className="text-shadow-lg">
-              NextApp
-            </a>
-          </h1>
-          <div className="nav-menu flex gap-4 text-gray-600">
-            <a
-              href="#"
-              className="hover:text-red-900 transition-all duration-500 ease-in-out p-1"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="hover:text-red-900 transition-all duration-500 ease-in-out p-1"
-            >
-              About
-            </a>
-            <a
-              href="#"
-              className="hover:text-red-900 transition-all duration-500 ease-in-out p-1"
-            >
-              myNextApp
-            </a>
-          </div>
+  return (
+    <nav className="navbar h-24 w-screen border shadow-sm">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <h1 className="font-bold ">
+          <a href="#" className="text-shadow">
+            NextApp
+          </a>
+        </h1>
+        <div className="nav-menu flex gap-4 text-gray-600 font-light">
+          <a
+            href="#"
+            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="hover:text-red-900 transition-all duration-500 ease-in-out text-shadow p-1"
+          >
+            myNextApp
+          </a>
         </div>
-      </nav>
-    );
-}
- 
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
