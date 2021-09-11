f9fcba89f4b942ab92fc41770054b82d4fc34ca4
farazzhh <havaee.faraz@gmail.com>
2021-09-12 01:37:53 +0430

    Footer

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <footer className="navbar h-24 w-screen border shadow-sm px-8">
+        <div className="container mx-auto flex justify-between items-center h-full">
+          <h1 className="">
+            <a href="#" className="text-shadow">
+              NextApp
+            </a>
+          </h1>
+
+          <p className="text-shadow">Copyright 2021 Farazzhh</p>
+          <a href="" className="text-shadow">
+            Github
+          </a>
+        </div>
+      </footer>
+    );
+}
+ 
+export default Footer;
