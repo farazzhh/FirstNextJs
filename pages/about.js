ab0cf4b1393fdcacdeef61f1ac659165b65f6bc9
farazzhh <havaee.faraz@gmail.com>
2021-09-11 23:00:31 +0430

    install tailwind

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="">about</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut labore cum molestias. Voluptatibus esse nobis, exercitationem aut nulla nemo laborum quidem quia nostrum tenetur saepe eaque eos debitis doloremque error!</p>
+        </div>
+    )
+}
+
+export default About
